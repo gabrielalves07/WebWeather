be733fceaded2dfaf94afe7bf9d7be39055a0520
Gabriel Alves <85041753+gabrielalves07@users.noreply.github.com>
2023-04-09 11:47:19 -0300

    alteração no tamanho da div container

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const bt_search = document.querySelector('#bt_search');
+
+bt_search.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('para de apertar nesse bagulho aí, meu nobre');
+});
