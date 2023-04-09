1e84be72e04f857d78b8be9de076fc141db791ba
Gabriel Alves <85041753+gabrielalves07@users.noreply.github.com>
2023-04-09 19:04:24 -0300

    adicionando loading

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
-const bt_search = document.querySelector('#bt_search');
+const form_search = document.querySelector('#search');
+
 const input = document.querySelector('#input');
 const city = document.querySelector('#city');
 const temp = document.querySelector('#temp');
@@ -8,7 +9,12 @@ const icon = document.querySelector('#icon');
 const wind = document.querySelector('#wind_text');
 const humidity = document.querySelector('#humidity_text');
 
-bt_search.addEventListener('click', e => {
+const div_conteudo = document.querySelector('#conteudo');
+const initial_div = document.querySelector('#initial_div');
+
+const loading = document.querySelector('#loading_div');
+
+form_search.addEventListener('submit', e => {
     e.preventDefault();
     if(input.value !== ''){
         searchWeather(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=ffdc4df60bcc770a3aeb414bc3d0859e&lang=pt_br&units=metric`);
@@ -16,7 +22,14 @@ bt_search.addEventListener('click', e => {
 });
 
 async function searchWeather(endpoint){
+    div_conteudo.style.display = 'none';
+    initial_div.style.display = 'none';
+    loading.style.display = 'flex';
+
     const response = await fetch(endpoint);
+    
+    loading.style.display = 'none';
+    div_conteudo.style.display = 'block';
     const data = await response.json();
 
     updateInfo(data.name, data.main.temp, data.sys.country, data.weather[0].description, data.weather[0].icon, data.wind.speed, data.main.humidity);
