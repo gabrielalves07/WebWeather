88f257e3fd4d29b8d08f3fad72a0e1c54b8268a7
Gabriel Alves <85041753+gabrielalves07@users.noreply.github.com>
2023-04-09 17:52:01 -0300

    finalização das requests

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,11 @@ const bt_search = document.querySelector('#bt_search');
 const input = document.querySelector('#input');
 const city = document.querySelector('#city');
 const temp = document.querySelector('#temp');
+const flag = document.querySelector('#flag');
+const desc = document.querySelector('#desc');
+const icon = document.querySelector('#icon');
+const wind = document.querySelector('#wind_text');
+const humidity = document.querySelector('#humidity_text');
 
 bt_search.addEventListener('click', e => {
     e.preventDefault();
@@ -14,11 +19,15 @@ async function searchWeather(endpoint){
     const response = await fetch(endpoint);
     const data = await response.json();
 
-    updateInfo(data.name, data.main.temp);
-    console.log(data);
+    updateInfo(data.name, data.main.temp, data.sys.country, data.weather[0].description, data.weather[0].icon, data.wind.speed, data.main.humidity);
 }
 
-function updateInfo(p_city, p_temp){
+function updateInfo(p_city, p_temp, p_flag, p_desc, p_icon, p_wind, p_humidity){
     city.innerHTML = p_city;
-    temp.innerHTML = `${p_temp.toFixed(1)} °C`;
+    temp.innerHTML = `${Math.round(p_temp)} °C`;
+    flag.src = `https://flagsapi.com/${p_flag}/flat/32.png`;
+    desc.innerHTML = p_desc;
+    icon.src = `http://openweathermap.org/img/wn/${p_icon}@2x.png`;
+    wind.innerHTML = `${p_wind} km/h`;
+    humidity.innerHTML = `${p_humidity}%`;
 }
